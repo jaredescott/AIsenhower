929f06844d819386487b4864f56c28d953929cde
Jared <jscott19@alum.mit.edu>
2024-05-03 03:19:41 -0400

    Refactor, reformat GUI

--- AIsenhower.jl
+++ AIsenhower.jl
@@ -1,133 +1,162 @@
-# AIsenhower.jl
-# 
-# Description: A simple Eisenhower matrix display for Reclaim.ai users
+"""
+A simple Eisenhower matrix display for Reclaim.ai users.
 
-API_KEY = "f83129f8-31fc-414c-8492-aca1d149b710" # Replace with your own API key from https://app.reclaim.ai/settings/developer
-# API_KEY = "<my_api_key>" # Replace with your own API key from https://app.reclaim.ai/settings/developer
+This code fetches tasks from the Reclaim.ai API and organizes them into an Eisenhower matrix, which categorizes tasks based on their urgency and importance. The matrix is displayed using a graphical user interface.
+
+Author: Jared Scott
+License: MIT
+"""
+
+# User-defined settings
+const API_KEY = "<my_api_key>" # Replace with your own API key from https://app.reclaim.ai/settings/developer
+const DARK_MODE = true
 
 
-DISPLAY_WINDOW = true
-API_URL = "https://api.app.reclaim.ai/api/tasks?status=NEW%2CSCHEDULED%2CIN_PROGRESS%2CCOMPLETE&instances=true"
-VERSION = "0.1.0"
+const API_URL = "https://api.app.reclaim.ai/api/tasks?status=NEW%2CSCHEDULED%2CIN_PROGRESS%2CCOMPLETE&instances=true"
+const VIEWPORT_HEIGHT_RATIO = 25/4
+const LABEL_WIDTH = 51
+const DEPENDENCIES = [
+    "HTTP",
+    "JSON",
+    "LiveServer"
+]
+
+using Pkg
+const installed_packages = Pkg.installed()
+is_installed(pkg::String) = haskey(installed_packages, pkg)
 
 function input(prompt::String)
     print(prompt)
     return readline()
 end
 
-install = input("Install dependencies? (y/N): ")
+@info "Loading dependencies..."
 
-if install in ["y", "Y"]
-    using Pkg
+if !is_installed("Mousetrap")
+    Pkg.add(url="https://github.com/clemapfel/mousetrap.jl")
+    Pkg.test("Mousetrap")
+end
 
-    begin
-        Pkg.add(url="https://github.com/clemapfel/mousetrap.jl")
-        Pkg.test("Mousetrap")
-    end
-
-    dependencies = [
-        "HTTP",
-        "JSON",
-        "LiveServer",
-        "PyCall"
-    ]
-
-    for dep in dependencies
-        Pkg.add(dep)
+for dependency in DEPENDENCIES
+    if !is_installed(dependency)
+        Pkg.add(dependency)
     end
 end
 
-
-using Mousetrap, HTTP, JSON#, PyCall
+using Mousetrap, HTTP
+using JSON: parse
 using LiveServer: open_in_default_browser
 
-if API_KEY == "<my_api_key>"
-    API_KEY = input("Please enter your API key: ")
+function get_tasks(url::String, token::String, _anonymous::Bool=false)
+    @info "Fetching tasks..."
+
+    headers = [
+        "Authorization" => "Bearer $token",
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+    ]
+
+    response = HTTP.get(url, headers)
+    tasks = parse(String(response.body))
+
+    if _anonymous
+        i = 0
+        for task in tasks
+            task["title"] = " "^40 * "Task $(i += 1)" * " "^40
+        end
+    end
+
+    # @info "Tasks fetched."
+    return tasks
 end
-
-headers = [
-    "Authorization" => "Bearer $API_KEY",
-    "Content-Type" => "application/json",
-    "Accept" => "application/json"
-]
-
-response = HTTP.get(API_URL, headers)
-
-tasks = JSON.parse(String(response.body))
-# println(tasks[1]) # sanity check
-
-task_properties = tasks[1] |> keys
-# println(task_properties)
 
 function reversed_dict(d::Dict)
     return Dict(v => k for (k, v) in d)
 end
 
-priority_to_action = Dict(
+const priority_to_action = Dict(
     "P1" => :DO,
     "P2" => :DECIDE,
     "P3" => :DELEGATE,
     "P4" => :DELETE
 )
-action_to_priority = reversed_dict(priority_to_action)
+const action_to_priority = reversed_dict(priority_to_action)
 
-task_matrix = Dict{Symbol, Array{Dict{String, Any}, 1}}(
-    :DO => [],
-    :DECIDE => [],
-    :DELEGATE => [],
-    :DELETE => []
+function partition_tasks(tasks)
+    @info "Organizing tasks..."
+    task_matrix = Dict{Symbol, Array{Dict{String, Any}, 1}}(
+        :DO => [],
+        :DECIDE => [],
+        :DELEGATE => [],
+        :DELETE => []
+    )
+
+    for task in tasks
+        priority = task["priority"]
+        # priority = task["onDeck"] ? "P1" : task["priority"] # Uncomment to prioritize "on deck" tasks
+        push!(task_matrix[priority_to_action[priority]], task)
+    end
+
+    return task_matrix
+end
+
+struct Tags
+    bold::String
+    italic::String
+    underline::String
+    monospace::String
+    small::String
+end
+const tags = Tags("b", "i", "u", "tt", "small")
+
+struct TextFormat
+    tag::Function
+    bold::Function
+    italic::Function
+    underline::Function
+    monospace::Function
+    small::Function
+    parenthesize::Function
+    color::Function
+    untag::Function
+    pad::Function
+end
+const format = TextFormat(
+    (text::String, tag::String) -> "<$tag>$text</$tag>",
+    (text::String) -> format.tag(text, tags.bold),
+    (text::String) -> format.tag(text, tags.italic),
+    (text::String) -> format.tag(text, tags.underline),
+    (text::String) -> format.tag(text, tags.monospace),
+    (text::String) -> format.tag(text, tags.small),
+    (text::String) -> "($text)",
+    (text::String, color::String) -> "<span color='$color'>$text</span>",
+    (tag::String) -> if tag[2] == "/" replace(tag, "/" => "") else replace(tag, "<" => "</") end,
+    (text::String, target_length::Int) -> "$(repeat(" ", round((target_length - length(text))/2)))$text$(repeat(" ", round((target_length - length(text))/2)))"
 )
 
-for task in tasks
-    priority = task["priority"]
-    priority = task["onDeck"] ? "P1" : task["priority"]
-    push!(task_matrix[priority_to_action[priority]], task)
-end
-
-# println(task_matrix) # sanity check
-
-bold = (text::String) -> "<b>$text</b>"
-italic = (text::String) -> "<i>$text</i>"
-underline = (text::String) -> "<u>$text</u>"
-big = (text::String) -> "<big>$text</big>"
-on_deck_font = (text::String) -> "<span color='green'>$text</span>" |> italic
-function antitag(tag::String)
-    if tag[2] == "/"
-        return replace(tag, "/" => "")
-    else
-        return replace(tag, "<" => "</")
-    end
-end
-# function normal(text::String)
-#     for tag in ["<b>", "</b>", "<i>", "</i>", "<u>", "</u>"]
-#         text = replace(text, tag => antitag(tag))
-#     end
-# end
-parenthesize = (text::String) -> "($text)"
 
 function generate_child(label::String)
-    out = Frame(Overlay(Separator(), Label(label)))
-    set_margin!(out, 3)
-    return out
+    matrix = Frame(Overlay(Separator(), Label(label)))
+    set_margin!(matrix, 3)
+    return matrix
 end
 
 function create_tasks_box(label, tasks)
-    out = Box(ORIENTATION_VERTICAL)
-    title_widget = Label(bold(String(label)))
-    push_back!(out, title_widget)
+    matrix = Box(ORIENTATION_VERTICAL)
+    set_expand!(matrix, true)
+    # title_widget = Label(format.pad((String(label)), LABEL_WIDTH) |> format.bold |> format.underline)
+    title_widget = Label("\n$(String(label) |> format.bold)\n")
+    push_back!(matrix, title_widget)
+    push_back!(matrix, Separator())
     for task in tasks
-        task_button = Button(Label(task["onDeck"] ? on_deck_font(task["title"]) : task["title"]))
+        task_button = Button(Label(task["onDeck"] ? (format.color(task["title"], "green")) : task["title"]))
         connect_signal_clicked!(task_button) do x::Button
             open_in_default_browser("https://app.reclaim.ai/tasks/$(task["id"])")
             nothing
         end
-        push_back!(out, task_button)
+        push_back!(matrix, task_button)
     end
-    return out
-    # c = ColumnView()
-    # c1 = push_back_column!(c, String(label))
-    # set_widget_at!(c, c1, 1, out)
-    # return c
+    return matrix
 end
 
 function add_css_classes!(widget, classes...)
@@ -141,29 +170,99 @@ function format_tasks_box(tasks_box, color=nothing)
     if color !== nothing
         add_css_class!(s, color)
     end
-    return Overlay(s, tasks_box)
+    set_expand!(s, true)
+    set_expand!(tasks_box, true)
+    o = Overlay(s, tasks_box)
+    set_expand!(o, true)
+    return o
 end
 
-function create_eisenhower_matrix(task_matrix)
-    out = Paned(ORIENTATION_HORIZONTAL)
-    out1, out2 = Paned(ORIENTATION_VERTICAL), Paned(ORIENTATION_VERTICAL)
-    sublabel_format = (text::String) -> text |> parenthesize |> italic
-    set_start_child!(out1, format_tasks_box(create_tasks_box("DO $(      action_to_priority[:DO] |>       sublabel_format)", task_matrix[:DO]),       "success"))
-    set_start_child!(out2, format_tasks_box(create_tasks_box("DECIDE $(  action_to_priority[:DECIDE] |>   sublabel_format)", task_matrix[:DECIDE]),   "warning"))
-    set_end_child!(  out1, format_tasks_box(create_tasks_box("DELEGATE $(action_to_priority[:DELEGATE] |> sublabel_format)", task_matrix[:DELEGATE]), "accent"))
-    set_end_child!(  out2, format_tasks_box(create_tasks_box("DELETE $(  action_to_priority[:DELETE] |>   sublabel_format)", task_matrix[:DELETE]),   "error"))
-    set_start_child!(out,  out1)
-    set_end_child!(  out,  out2)
-    return out
+function eisenhower_label(text::String, opacity::Float64=0.5)
+    label = Label(format.bold(text))
+    set_opacity!(label, opacity)
+    return label
 end
 
-if DISPLAY_WINDOW
-    main() do app::Application
-        window = Window(app)
-        set_title!(window, "AIsenhower v$VERSION")
+main() do AIsenhower::Application
+    # set theme
+    DARK_MODE && set_current_theme!(AIsenhower, THEME_DEFAULT_DARK)
 
-        eisenhower_matrix = create_eisenhower_matrix(task_matrix)
-        set_child!(window, eisenhower_matrix)
-        present!(window)
+    window = Window(AIsenhower)
+    set_expand!(window, true)
+    set_title!(window, "AIsenhower")
+
+    api_key = API_KEY == "<my_api_key>" ? input("Please enter your API key: ") : API_KEY
+    tasks = get_tasks(API_URL, api_key, true)[1:30]
+    task_matrix = partition_tasks(tasks)
+    sublabel_format = (text::String) -> text |> format.parenthesize |> format.italic
+
+    @info "Preparing Eisenhower matrix..."
+
+    # create column view
+    matrix = ColumnView()
+    set_expand!(matrix, true)
+    column = push_back_column!(matrix, " ") # empty column
+
+    viewports = Dict(
+        :DO => Viewport(),
+        :DECIDE => Viewport(),
+        :DELEGATE => Viewport(),
+        :DELETE => Viewport()
+    )
+    set_expand!.(values(viewports), true)
+    min_height = maximum(length.(t["title"] for t in tasks)) * VIEWPORT_HEIGHT_RATIO
+    println(min_height)
+    for viewport in values(viewports)
+        set_size_request!(viewport, Vector2f(0, min_height/2))
     end
+    
+    function set_children()
+        set_child!(viewports[:DO], format_tasks_box(create_tasks_box("DO $(action_to_priority[:DO] |> sublabel_format)", task_matrix[:DO]), "success"))
+        set_child!(viewports[:DECIDE], format_tasks_box(create_tasks_box("DECIDE $(action_to_priority[:DECIDE] |> sublabel_format)", task_matrix[:DECIDE]), "warning"))
+        set_child!(viewports[:DELEGATE], format_tasks_box(create_tasks_box("DELEGATE $(action_to_priority[:DELEGATE] |> sublabel_format)", task_matrix[:DELEGATE]), "accent"))
+        set_child!(viewports[:DELETE], format_tasks_box(create_tasks_box("DELETE $(action_to_priority[:DELETE] |> sublabel_format)", task_matrix[:DELETE]), "error"))
+    end
+
+    set_children()
+
+    # column 1: Important/Less Important
+    column = push_back_column!(matrix, " ")
+    important_label = eisenhower_label("Important")
+    set_widget_at!(matrix, column, 2, important_label)
+    unimportant_label = eisenhower_label("Less Important")
+    set_widget_at!(matrix, column, 3, unimportant_label)
+
+    # column 2: Urgent
+    column = push_back_column!(matrix, " ")
+    urgent_label = eisenhower_label("Urgent")
+    set_widget_at!(matrix, column, 1, urgent_label)
+    set_widget_at!(matrix, column, 2, viewports[:DO])
+    set_widget_at!(matrix, column, 3, viewports[:DELEGATE])
+    
+    # column 3: Not Urgent
+    column = push_back_column!(matrix, " ")
+    not_urgent_label = eisenhower_label("Not Urgent")
+    set_widget_at!(matrix, column, 1, not_urgent_label)
+    set_widget_at!(matrix, column, 2, viewports[:DECIDE])
+    set_widget_at!(matrix, column, 3, viewports[:DELETE])
+
+    # refresh button
+    column = push_back_column!(matrix, " ")
+    refresh_button = Button(Label("&#8635;"))
+    set_opacity!(refresh_button, 0.5)
+    connect_signal_clicked!(refresh_button) do x::Button
+        @info "Refreshing..."
+        task_matrix = partition_tasks(get_tasks(API_URL, api_key))
+        set_children()
+        @info "Tasks reloaded."
+    end
+    set_widget_at!(matrix, column, 4, refresh_button)
+
+    set_propagate_natural_width!.(values(viewports), true)
+
+    set_child!(window, matrix)
+    
+    @info "Displaying Eisenhower matrix..."
+    present!(window)
 end
+
--- README.md
+++ README.md
@@ -6,9 +6,32 @@ This Julia app displays all of a user's incomplete tasks from the AI planning we
   > Priority 2 - Not Urgent, but Important<br>
   >  Priority 3 - Urgent, but Not Important<br>
   > Priority 4 - Not Urgent, Not Important<br>
+
+The app allows users to view and interact with their tasks in a succinct visual manner, making it easier to prioritize tasks and plan their day. This is especially useful for users who (like myself) have a large number of tasks on Reclaim and would like to view them all at once in a single window.
+
+<!-- | AIsenhower (Light Mode) |:-:| AIsenhower (Dark Mode) |
+|-------------------------|:-:|------------------------| -->
+     ![AIsenhower Screenshot - Light Mode](images/example_light.png)            ![AIsenhower Screenshot - Dark Mode](images/example_dark.png)
   
 ## Usage Notes
- - Before running the Julia script, obtain an API key from https://app.reclaim.ai/settings/developer and copy it into the `API_KEY` variable at the top of the script.
- - Run "AIsenhower.jl" to open the app.
- - When running the script for the first time, type "y" into the terminal/REPL when prompted to install dependencies.
- - Clicking on a task will open the task in the default browser.
+
+- Before running the Julia script, obtain an API key from https://app.reclaim.ai/settings/developer and copy it into the `API_KEY` variable at the top of the script. The `DARK_MODE` variable can also be set to `true` or `false` depending on the user's preference.
+
+- Run `julia <path_to_script_directory>/AIsenhower.jl` in the terminal or `include("<path_to_script_directory>/AIsenhower.jl")` in the Julia REPL to start the app. The script will stop running when the GUI window is closed.
+
+- Tasks marked "Up Next" in Reclaim are displayed with a green text color on the matrix.
+
+- Lists of tasks longer than their display box are scrollable.
+
+- Clicking on a task will open the task in the default browser.
+
+- Clicking the refresh symbol () at the bottom right of the app reloads the tasks from the Reclaim API.
+
+## Credits
+
+  - The [Julia Programming Language](https://julialang.org/) for the language and package manager.
+  - [Reclaim.ai](https://app.reclaim.ai) for the API to access tasks.
+  - [Mousetrap.jl](https://github.com/Clemapfel/mousetrap.jl) for the GUI engine.
+
+---
+To ask questions or provide comments regarding this application, navigate to this GitHub repo's Discussion page or email [jscott19@alum.mit.edu](mailto:jscott19@alum.mit.edu). For feature requests or bug reports, feel free to create an issue on the GitHub repo's Issues page.
